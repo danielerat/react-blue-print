66ee4a0d640f062306693a581611045bc7e4bf6c
danielerat <davidodo2015@gmail.com>
2023-09-23 13:13:26 +0200

    ReactApp Boiler Plate code With Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-blue-400 font-bold text-3xl h-screen flex justify-center items-center">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
